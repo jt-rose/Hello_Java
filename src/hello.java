82de1bd53645487ec3a4c6d6e4277431f1e6acd8
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-11 00:12:42 -0400

    set up hello java

---
+++ src/hello.java
@@ -0,0 +1,14 @@
+public class hello {
+    public static void main(String[] args) {
+    // hello Java!
+        System.out.println("Hello Java!");
+
+        // variables
+        String name = "Jeff";
+        if (name.equals("Jeff")) {
+            System.out.println("Hello " + name);
+        } else {
+            System.out.println("...who dis?");
+        }
+    }
+}
